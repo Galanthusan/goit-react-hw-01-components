238df9e24a25be72751a79039a5d72b4512b7fb3
lesogorovalex <galanthusan@gmail.com>
2020-09-16 16:27:23 +0300

    Updates

---
+++ precache-manifest.84672486409afc2b00f4647450cb2213.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55daa70f07710c39cffe555252f3b70d",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6a427a53e9d406f2b8ec",
+    "url": "/goit-react-hw-01-components/static/css/main.0872da2b.chunk.css"
+  },
+  {
+    "revision": "7fc5c9de265a5ddab8ea",
+    "url": "/goit-react-hw-01-components/static/js/2.fa925144.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.fa925144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a427a53e9d406f2b8ec",
+    "url": "/goit-react-hw-01-components/static/js/main.593d6860.chunk.js"
+  },
+  {
+    "revision": "847830f7d7918ee14cd1",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.1f771674.js"
+  },
+  {
+    "revision": "50475e9398305457c4ead55941b10a0f",
+    "url": "/goit-react-hw-01-components/static/media/helveticaneuecyr-bold.50475e93.ttf"
+  },
+  {
+    "revision": "307781839e3e303ff49b223dc242baac",
+    "url": "/goit-react-hw-01-components/static/media/helveticaneuecyr-light.30778183.ttf"
+  }
+]);
